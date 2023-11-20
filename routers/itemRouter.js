fd602176269955a42836a7c23e7c660a3f3f39eb
tdedwin <nuttawut.c39@gmail.com>
2023-11-20 17:12:06 +0700

    add option

--- routers/itemRouter.js
+++ routers/itemRouter.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-const URL_MARKET = 'https://apps.maxion.gg/api/market/list?status=LISTING&serverId=1'
+const URL_MARKET = 'https://apps.maxion.gg/api/market/list?status=LISTING&'
+const server = ['serverId=1', 'serverId=2']
 const BASE_URL_Img = 'https://apps.maxion.gg/_next/image?url=https%3A%2F%2Frop2e-collection-cdn.s3-bkk.nipa.cloud%2F'
 const buy_url = 'https://apps.maxion.gg/roverse/detail/'
 let category = ['all','headgear', 'weapon', 'armor', 'card', 'shadowgear', 'costume']
 let weapon_type = ['all','Katar','Bow','Mace','1hSword','2hSpear','2hSword']
+let mode_search = ['new', 'low', 'high']
 
 router.post('/get_item', async (req,res) => {
-    let {indexCategory , indexWeapon} = req.body
+    let {indexCategory , indexWeapon, indexSearch, indexServer} = req.body
     let mode = category[indexCategory] || category[1]
     let mode_weapon = weapon_type[indexWeapon] || weapon_type[0]
+    let search = mode_search[indexSearch] || mode_search[0]
+    let sv = server[indexServer] || server[0]
     let response = []
     let list = []
     let sort = []
     try {
         switch (mode) {
             case 'all':
-                response = await axios.get(`${URL_MARKET}`)
+                response = await axios.get(`${URL_MARKET}${sv}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -25,40 +29,68 @@ router.post('/get_item', async (req,res) => {
                     })
                 }
                 for(let i = 0; i < response.data.length; i++){
-                    if(response.data[i].nft.refine >= 7 && !!response.data[i].nft.option0Text){
-                        let obj = {
-                            img : `${BASE_URL_Img}${response.data[i].nft.nameid}.png&w=256&q=75`,
-                            name : response.data[i].nft.nameEnglish,
-                            price : Number(response.data[i].price),
-                            refine : response.data[i].nft.refine,
-                            option1 : response.data[i].nft.option0Text,
-                            option2 : response.data[i].nft.option1Text,
-                            option3 : response.data[i].nft.option2Text,
-                            option4 : response.data[i].nft.option3Text,
-                            option5 : response.data[i].nft.option4Text,
-                            url : `${buy_url}${response.data[i].id}`
-                        }
-                        if(!!response.data[i].nft.card0Name){
-                            obj.card1 = response.data[i].nft.card0Name
-                        }
-                        if(!!response.data[i].nft.card1Name){
-                            obj.card2 = response.data[i].nft.card1Name
-                        }
-                        if(!!response.data[i].nft.card2Name){
-                            obj.card3 = response.data[i].nft.card2Name
-                        }
-                        list.push(obj)
+                    let obj = {
+                        img : `${BASE_URL_Img}${response.data[i].nft.nameid}.png&w=256&q=75`,
+                        name : response.data[i].nft.nameEnglish,
+                        price : Number(response.data[i].price),
+                        url : `${buy_url}${response.data[i].id}`
                     }
-                    sort = list.sort((a,b) => {
-                        return a.price - b.price
-                    })
+                    if(!!response.data[i].nft.option0Text){
+                        obj.option1 = response.data[i].nft.option0Text
+                    }
+                    if(!!response.data[i].nft.option1Text){
+                        obj.option2 = response.data[i].nft.option1Text
+                    }
+                    if(!!response.data[i].nft.option2Text){
+                        obj.option3 = response.data[i].nft.option2Text
+                    }
+                    if(!!response.data[i].nft.option3Text){
+                        obj.option4 = response.data[i].nft.option3Text
+                    }
+                    if(!!response.data[i].nft.option4Text){
+                        obj.option5 = response.data[i].nft.option4Text
+                    }
+                    if(!!response.data[i].nft.refine){
+                        obj.refine = response.data[i].nft.refine
+                    }
+                    if(!!response.data[i].nft.card0Name){
+                        obj.card1 = response.data[i].nft.card0Name
+                    }
+                    if(!!response.data[i].nft.card1Name){
+                        obj.card2 = response.data[i].nft.card1Name
+                    }
+                    if(!!response.data[i].nft.card2Name){
+                        obj.card3 = response.data[i].nft.card2Name
+                    }
+                    list.push(obj)
                 }
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'headgear':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -90,16 +122,34 @@ router.post('/get_item', async (req,res) => {
                         }
                         list.push(obj)
                     }
-                    sort = list.sort((a,b) => {
-                        return a.price - b.price
-                    })
                 }
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'weapon':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -134,9 +184,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -166,9 +213,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -198,9 +242,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -230,9 +271,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -262,9 +300,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -294,9 +329,6 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 
@@ -326,18 +358,36 @@ router.post('/get_item', async (req,res) => {
                                 }
                                 list.push(obj)
                             }
-                            sort = list.sort((a,b) => {
-                                return a.price - b.price
-                            })
                         }
                         break;
                 }
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'armor':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -369,16 +419,34 @@ router.post('/get_item', async (req,res) => {
                         }
                         list.push(obj)
                     }
-                    sort = list.sort((a,b) => {
-                        return a.price - b.price
-                    })
                 }
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'card':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -394,15 +462,33 @@ router.post('/get_item', async (req,res) => {
                     }
                     list.push(obj)
                 }
-                sort = list.sort((a,b) => {
-                    return a.price - b.price
-                })
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'shadowgear':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -429,16 +515,34 @@ router.post('/get_item', async (req,res) => {
                         }
                         list.push(obj)
                     }
-                    sort = list.sort((a,b) => {
-                        return a.price - b.price
-                    })
                 }
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             case 'costume':
-                response = await axios.get(`${URL_MARKET}&category=${mode}`)
+                response = await axios.get(`${URL_MARKET}${sv}&category=${mode}`)
                 if(!response.data || response.data.length <= 0){
                     throw({
                         msg : 'ไม่พบข้อมูล ลองใหม่อีกครั้ง',
@@ -456,12 +560,30 @@ router.post('/get_item', async (req,res) => {
                         list.push(obj)
                     }
                 }
-                sort = list.sort((a,b) => {
-                    return a.price - b.price
-                })
-                res.send({
-                    data : sort
-                })
+                switch (search) {
+                    case 'low':
+                        sort = list.sort((a,b) => {
+                            return a.price - b.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                    case 'high':
+                        sort = list.sort((a,b) => {
+                            return b.price - a.price
+                        })
+                        res.send({
+                            data : sort
+                        })
+                        break;
+                
+                    default:
+                        res.send({
+                            data : list
+                        })
+                        break;
+                }
                 break;
             default:
                 break;
